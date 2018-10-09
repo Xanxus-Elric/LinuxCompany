1d8994b5303d8830a2a458cbaf9418ed58d4d327
Xanxus-Elric <603819992@qq.com>
2018-10-09 17:18:43 +0800

    FirstTestMessage

---
+++ Test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: Test.c
+	> Author: 
+	> Mail: wangrq@farsight.com.cn
+	> Created Time: Tue 09 Oct 2018 05:17:30 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
